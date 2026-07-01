0cc8dd2e5e31f82cbd7842ca2ff32916627a5e0b
Akashvenkatesh-347 <akashvenkatesh347@gmail.com>
2026-07-01 21:04:36 +0530

    feat: added subscription setters

--- Subscription.java
+++ Subscription.java
@@ -24,6 +24,23 @@ public class Subscription {
         return autoRenew;
     }
 
+    //Setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setMonthlyCost(double monthlyCost){
+        this.monthlyCost = monthlyCost;
+    }
+    public void setBillingDate(int billingDate) {
+        this.billingDate = billingDate;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setAutoRenew(boolean autoRenew) {
+        this.autoRenew = autoRenew;
+    }
+
     //Constructor
     public Subscription(String name,double monthlyCost,int billingDate,String category,boolean autoRenew){
         this.name = name;
